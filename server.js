4b2c617d2dbce1dea0b375dbe8435502b2b8eead
Andela-JUdensi <joshua.udensi@andela.com>
2017-11-07 07:40:27 +0100

    ft(sorting): sort blocker and implement http2 - sorting - http2 - house keeping

--- server.js
+++ server.js
@@ -8,9 +8,20 @@ import { blockersRoute, usersRoute } from './routes';
 import { Response } from './utils';
 import { configs } from './configs';
 import { subscribeHandlers } from './events';
+import spdy from 'spdy';
+import fs from 'fs';
+import HE from 'he-sdk-nodejs';
 
 const PORT = process.env.PORT || 5001;
 const server = express();
+const sslOptions = {
+  key: fs.readFileSync(__dirname + '/server.key'),
+  cert:  fs.readFileSync(__dirname + '/server.crt'),
+  spdy: {
+    protocols: ['h2', 'spdy/3.1', 'http/1.1'],
+    plain: false,
+  }
+};
 
 server.use(compression());
 server.use(cors({
@@ -30,12 +41,29 @@ server.all('*', (req, res, next) => {
   next();
 });
 
+server.post('/compile', (req, res) => {
+  const source = `import sys;print(sys.path)`;
+  const settings = {
+    'client_secret': '664e9b3a4dcc4810e8c8a1b262a1e11a5bd94056' ,
+    'async': 0 ,
+    'lang': 'PYTHON' ,
+    'time_limit': 5,
+    'memory_limit': 262144
+  };
+
+  HE.run(settings , source , function(err , result){
+    if (err) return Response.badRequest(res, err);
+    Response.success(res, JSON.parse(result));
+  });
+});
+
 server.get('*', (req, res) => {
   Response.success(res, 'Cognitio | Knowledge Enterprise | Built for Devs. by Devs.')
 });
 
-server.listen(PORT, () =>
-  logger.info(`Cognitio API server started on PORT ${PORT}`.green));
+spdy.createServer(sslOptions, server)
+  .listen(PORT, () =>
+    logger.info(`Cognitio API server started on PORT ${PORT}`.green));
 
 subscribeHandlers();
 
