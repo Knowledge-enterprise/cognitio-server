e76ca16cc934f926a3d05a9b0e653fc0ed1a55c7
Anthony Ngene <anthony.ngene@andela.com>
2017-11-17 15:34:56 +0100

    add user tests

---
+++ test/testData.js
@@ -0,0 +1,6 @@
+export const testUser = {
+  uid: "5a03f35120178e0e233b2f6f",
+  name: "Montaro Zen",
+  email: "kenpachi@bleach.com",
+  picture: "http://www.quanmax.com/site/wp-content/uploads/2017/06/test.png"
+};
